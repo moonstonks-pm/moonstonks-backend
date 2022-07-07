fd71f57b18b18e468ad429e5ad971f8307b41277
nicobrauchtgit <103033288+nicobrauchtgit@users.noreply.github.com>
2022-07-07 11:59:10 +0200

    Update PortfolioAnalyse.java

--- src/main/java/hwr/oop/PortfolioAnalyse.java
+++ src/main/java/hwr/oop/PortfolioAnalyse.java
@@ -2,4 +2,28 @@ package hwr.oop;
 
 public class PortfolioAnalyse {
 
+    private Portfolio portfolio;
+    private HashMap<String, Double> positionValue;
+
+    private double portfolioValue;
+    PortfolioAnalyse(Portfolio portfolio) {
+        this.portfolio = portfolio;
+        this.positionValue = portfolio.getPositionValues();
+        this.portfolioValue = portfolio.value();
+
+    }
+
+    public String SecurityTypeAllocation(){
+        HashMap<String, Double> securityTypeAllocation = new HashMap<>();
+        for(String key: positionValue.keySet()) {
+            if(!securityTypeAllocation.containsKey(ParseShareMetaData.getSecurityType(key))){
+                securityTypeAllocation.put(ParseShareMetaData.getSecurityType(key), positionValue.get(key));
+            }else{
+                securityTypeAllocation.computeIfPresent(ParseShareMetaData.getSecurityType(key),
+                        (securityType, overallValue) -> overallValue += positionValue.get(key));
+            }
+        }
+        return securityTypeAllocation.toString();
+    }
+
 }
