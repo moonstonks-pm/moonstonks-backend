398e1b6e64bb4db83e34f4240ebb85ce551a9f95
Tom Sonntag <tom.sonntag@sap.com>
2022-07-07 13:22:49 +0200

    Fix: refactored stock structure

--- src/main/java/hwr/oop/ShareMetaData.java
+++ src/main/java/hwr/oop/ShareMetaData.java
@@ -1,5 +1,5 @@
 package hwr.oop;
-
+//TODO DAI neu
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
--- src/main/java/hwr/oop/StockSearch.java
+++ src/main/java/hwr/oop/StockSearch.java
@@ -5,21 +5,18 @@ import java.util.HashSet;
 
 public class StockSearch {
 
-    private static final String dateToday = "2022-07-01";  //in Order to run Tests with JSON Files the current date is hardcoded. When using an API this could be changed so that the current date is fetched.
     private static final String earliestDatePossible = "2022-02-01";
-    private static final String[] securitiesAvailableArray = new String[]{"SAP", "IBM", "DAI", "EUNL", "IS3N", "SXRJ"};
+    private static final String[] securitiesAvailableArray = new String[]{"SAP", "IBM", "MBG", "EUNL", "IS3N", "SXRJ"};
 
     private static final HashSet<String> securitiesAvailable =
             new HashSet<>(Arrays.asList(securitiesAvailableArray));
 
     private final String security;
-    private final String currentDate;
     private final String earliestDate;
 
 
     StockSearch(String securityAcronym){
         this.security = securityAcronym.toUpperCase();
-        this.currentDate = dateToday;
         this.earliestDate = earliestDatePossible;
     }
     public boolean securityIsAvailable(){
